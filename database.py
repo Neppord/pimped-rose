59d16e71abcda9dbe5bb89a2c736d70107b498b6
Samuel Ytterbrink <say@praqma.net>
2019-02-15 11:39:51 +0100

    Improve the database, and make it more fun!

--- database.py
+++ database.py
@@ -1,13 +1,26 @@
 from itertools import product
 from random import choice, sample
 
-
-NAMES = [
-    "Master sword",
-    "Oger Club Card",
-    "Majors Mask",
-    "Ocarina of Mime"
+PREFIXES = [
+    "Master",
+    "Oger",
+    "Majors",
+    "Ocarina",
+    "Sword",
+    "Textbook",
+    "Worn",
 ]
+
+POSTFIXES = [
+    "Shield",
+    "Sword",
+    "Club Card",
+    "Mask",
+    "of Mime",
+    "of Stone",
+]
+
+NAMES = [pre + " " + post for pre, post in product(PREFIXES, POSTFIXES)]
 QUALITIES = list(range(50))
 SELL_INS = list(range(360))
 
