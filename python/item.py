01634bb68914b41d480231778186b811e7ca45da
Samuel Ytterbrink <neppord@gmail.com>
2019-04-23 17:48:20 +0200

    Fix formatting to be pep compliant.

--- python/item.py
+++ python/item.py
@@ -1,6 +1,6 @@
 class Item:
 
-    def __init__(self, name:str, quality:int, sell_in:int):
+    def __init__(self, name: str, quality: int, sell_in: int):
         self.name = name
         self.quality = quality
         self.sell_in = sell_in
--- python/report.py
+++ python/report.py
@@ -4,16 +4,18 @@ from pricing import PricingRegistry
 
 class Report:
 
-    def __init__(self, items:[Item]):
+    def __init__(self, items: [Item]):
         self.items = items
         self.pricing_registry = PricingRegistry()
 
     def __str__(self):
-        return (
-            f"{'name': <20}{'quality': >10}{'sell in': >10}{'price': >10}\n" +
-            "\n".join(
-            f"{item.name: <20}{item.quality: >10}{item.sell_in: >10}{self.pricing_registry.get_price(item): >10.2f}"
+        table = "\n".join(
+            f"{item.name: <20}" +
+            f"{item.quality: >10}" +
+            f"{item.sell_in: >10}" +
+            f"{self.pricing_registry.get_price(item): >10.2f}"
             for item in self.items
-            )
         )
+        header = f"{'name': <20}{'quality': >10}{'sell in': >10}{'price': >10}\n"
+        return header + table
 
